d30a25a389f28071b117d5192248285329824b54
Buntry <parkergriep@yahoo.com>
2015-08-06 18:58:10 -0400

    lab completed

--- ApplesAndHolidays/FISAppDelegate.m
+++ ApplesAndHolidays/FISAppDelegate.m
@@ -16,10 +16,36 @@
     return YES;
 }
 
-/**
- 
- * Implement your methods here.
- 
- */
+-(NSArray *)pickApplesFromFruits:(NSArray *)fruits {
+    return [fruits filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"self == %@", @"apple"]];
+}
+
+-(NSArray *)holidaysInSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    return [database[season] allKeys];
+}
+
+-(NSArray *)suppliesInHoliday:(NSString *)holiday inSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    return database[season][holiday];
+}
+
+-(BOOL)holiday:(NSString *)holiday isInSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    return [database[season] objectForKey:holiday];
+}
+
+-(BOOL)supply:(NSString *)supply isInHoliday:(NSString *)holiday inSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    return [database[season][holiday] containsObject:supply];
+}
+
+-(NSDictionary *)addHoliday:(NSString *)holiday toSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    if (![[database[season] allKeys] containsObject:holiday])
+        database[season][holiday] = [@[] mutableCopy];
+
+    return database;
+}
+
+-(NSDictionary *)addSupply:(NSString *)supply toHoliday:(NSString *)holiday inSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    [database[season][holiday] addObject:supply];
+    return database;
+}
 
 @end
